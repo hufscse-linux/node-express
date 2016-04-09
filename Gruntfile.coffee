6c57ef6d5a15a57a468031179a88c7f5b70f4337
Dongseob Park <dongseob.park@gmail.com>
2016-04-09 18:37:09 +0900

    add mongoose and some configuration for development

--- Gruntfile.coffee
+++ Gruntfile.coffee
@@ -15,6 +15,15 @@ module.exports = (grunt) ->
         tasks: ['express:dev']
         options:
           spawn: false
+    mkdir:
+      mongodb:
+        options:
+          mode: 777
+          create: ['tmp/mongo-data']
+    bgShell:
+      local_mongodb:
+        cmd: "mongod --dbpath=" + __dirname + "/tmp/mongo-data"
+        bg: true
     uglify:
       options:
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -25,8 +34,12 @@ module.exports = (grunt) ->
   grunt.loadNpmTasks 'grunt-contrib-uglify'
   grunt.loadNpmTasks 'grunt-express-server'
   grunt.loadNpmTasks 'grunt-contrib-watch'
+  grunt.loadNpmTasks 'grunt-mkdir'
+  grunt.loadNpmTasks 'grunt-bg-shell'
   
-  
-  grunt.registerTask 'default', ['express:dev', 'watch']
+  grunt.registerTask 'default', [
+    'mkdir:mongodb', 'bgShell:local_mongodb'
+    'express:dev', 'watch'
+  ]
   grunt.registerTask 'production', ['uglify', 'express:production']
 
--- src/app.js
+++ src/app.js
@@ -1,38 +1,48 @@
 'use strict';
 
-var express = require('express');
-var path    = require("path");
+var mongoose = require('mongoose');
+var express  = require('express');
+var path     = require("path");
+
+var Post = require("./model/post");
 
 var app = express();
 app.set('view engine', 'jade');
 
+mongoose.connect("mongodb://localhost/test");
+
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Title',
-        message: 'Message',
-        line: "This is Line",
-        array: [
-            "first", "second", "third", "forth"
-        ]
+
+    Post.find(function(err, entries) {
+        var data = entries;
+        res.render('index', {
+            data: data,
+            title: 'Title',
+            message: 'Message',
+            line: "This is Line",
+            array: [
+                "first", "second", "third", "forth"
+            ]
+        });
     });
-    //res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.get('/test12', function (req, res) {
-    res.render('test', {});
-});
-
-app.get('/test123', function (req, res) {
-    res.render('test123', {});
-});
-
-app.get('/test', function (req, res) {
-    res.send({
-        key: "test",
-        value: "testdata"
+app.get('/generate', function(req, res) {
+    var post = new Post({
+        title: "New Post", body: "Blank"
     });
+    post.save(function(err) {
+        console.log(err);
+    });
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+
+
+
+
+
