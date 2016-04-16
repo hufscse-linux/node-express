d6d11553fad36a96eabfa59352fb4d14e5decbc9
Dongseob Park <dongseob.park@gmail.com>
2016-04-16 20:58:45 +0900

    signin

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,22 @@ var gulp  = require('gulp'),
     bg    = require('gulp-bg'),
     mkdirp= require('mkdirp');
 
+
+
 gulp.task('mkdir-local-mongodb', function() { mkdirp('tmp/mongo-data'); });
 gulp.task('run-local-mongodb', ['mkdir-local-mongodb'],
           bg("mongod", "--dbpath=" + __dirname + "/tmp/mongo-data"));
+gulp.task('run-local-redis',
+          bg("redis-server"));
 
+gulp.task(
+    'default',
+    ['run-local-mongodb', "run-local-redis"],
+    function() {
+        var server = gls.new('src/app.js');
+        server.start();
 
-gulp.task('default', ['run-local-mongodb'], function() {
-
-    var server = gls.new('src/app.js');
-    server.start();
-
-    gulp.watch('src/app.js', function() {
-        server.start.bind(server);
+        gulp.watch('src/app.js', function() {
+            server.start.bind(server);
+        });
     });
-});
--- src/app.js
+++ src/app.js
@@ -3,46 +3,74 @@
 var mongoose = require('mongoose');
 var express  = require('express');
 var path     = require("path");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var RedisStore = require("connect-redis")(session);
 
 var Post = require("./model/post");
 
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(
+    session({
+        store: new RedisStore(),
+        secret: 's3cret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 30000 }
+    })
+);
+
 app.set('view engine', 'jade');
 
 mongoose.connect("mongodb://localhost/test");
 
 app.get('/', function (req, res) {
+    var params = {};
 
-    Post.find(function(err, entries) {
-        var data = entries;
-        res.render('index', {
-            data: data,
-            title: 'Title',
-            message: 'Message',
-            line: "This is Line",
-            array: [
-                "first", "second", "third", "forth"
-            ]
-        });
-    });
+    if(req.session.username) {
+        params["username"] = req.session.username;
+    }
+    
+    res.render('index', params);
 });
 
-app.get('/generate', function(req, res) {
-    var post = new Post({
-        title: "New Post", body: "Blank"
-    });
-    post.save(function(err) {
-        console.log(err);
-    });
+app.get('/signin', function (req, res) {
+    res.render('signin', {});
+});
+
+var users = [
+    { username: 'tomcat', password: 's3cret' },
+    { username: 'tiger', password: 'scott' }
+];
+
+app.post('/signin', function (req, res) {
+    var found = false;
+    var ui = req.body;
+    for(var i in users) {
+        if((users[i].username == ui.username) &&
+           (users[i].password == ui.password)) {
+            found = true;
+            break;
+        }
+    }
+    if(found) {
+        req.session.username = ui.username;
+        res.redirect("/");
+    } else {
+        res.render("signin", {
+            errorMessage: "Can't sign in"
+        });
+    }
+});
+
+app.get('/signout', function(req, res) {
+    req.session.destroy();
     res.redirect("/");
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
-
-
-
-
-
-
